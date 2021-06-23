f8eb5a9436e382f2bc87830b782cda36a8c2039a
Bharat Chilampuram <bharat3234@gmail.com>
2021-06-23 10:04:06 +0530

    angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-const routes: Routes = [];
+// route registration
+const routes: Routes = [
+  {
+    path: 'contact',
+    component: ContactUsComponent
+  },
+  {
+    path: 'about',
+    component: AboutUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+// Angular Routing
+/*
+to navigate from one page to another page
+*/
